722f6c2af95351739e35e8890a553d8038406e90
Peter Csiba <petherz@gmail.com>
2020-05-13 22:17:03 -0700

    [comtrade] Refactor into more re-usable parts by clearly separating driver (comtrade_client) from the scraper (main)

--- comtrade/comtrade_client.py
+++ comtrade/comtrade_client.py
@@ -1,11 +1,23 @@
-# TODO: Explore/Implement BULK download, since we want to get all of the Data:
-#       https://comtrade.un.org/data/doc/api/bulk/#DataRequests
-#       Notice: this API is only available to users with a premium site license.
-# TODO: Explore https://comtrade.un.org/ for other uses.
-# TODO: Manage to get all the data (learn to run from multiple IPs).
+"""
+comtrade_client implements a subset of UNs Public API https://comtrade.un.org/data/doc/api/#APIKey
+
+BEWARE of the following usage limits:
+- Rate limit (guest): 1 request every second (per IP address or authenticated user).
+- Rate limit (authenticated): 1 request every second (per IP address or authenticated user).
+- Usage limit (guest): 100 requests per hour (per IP address or authenticated user).
+- Usage limit (authenticated): 10,000 requests per hour (per IP address or authenticated user).
+
+Parameter combination limits:
+- ps, r and p are limited to 5 codes each.
+- Only one of the above codes may use the special ALL value in a given API call.
+- Classification codes (cc) are limited to 20 items. ALL is always a valid classification code.
+
+NOTE:
+- BULK Download API is only available to users with a premium site license (would take <1h to download).
+"""
+
 # TODO: Somehow get the Authentication Token, "You don't have enough right to access this page ... "
 #       https://comtrade.un.org/data/doc/api/#APIKey
-# TODO: Long-term:Make it recover from machine suspense/failures
 
 import json
 import logging
@@ -20,8 +32,12 @@ from utils import MMEnum
 LOGGER = logging.getLogger("ComtradeClient")
 MIN_YEAR = 1961
 MAX_YEAR = 2019
+# Hard limit for /get/, even if max set, if the results set is over it, it will 5003: ComtradeResultTooLarge
+ROW_LIMIT = 100000
+API_BASE_URL = "https://comtrade.un.org/api/get"
 
 
+# ============== EXCEPTIONS and ERROR codes ========================
 class ComtradeException(Exception):
     pass
 
@@ -61,241 +77,169 @@ class ComtradeInvalidParameters(ComtradeException):
         return [5002, 5004]
 
 
-class ComtradeClient:
+# ============== ENUMS ========================
+class Period(MMEnum):
+    """ You can also pass in "YYYY", "YYYYMM" like 201911"""
+    NOW = "now"
+    RECENT = "recent"
+    ALL = "all"
+
+    @staticmethod
+    def generate_years(start=MIN_YEAR, end=MAX_YEAR):
+        return [str(year) for year in range(start, end+1, 1)]
+
+    @staticmethod
+    def generate_yearmonths(start_year=MIN_YEAR, end_year=MAX_YEAR):
+        result = []
+        for year in Period.generate_years(start_year, end_year):
+            result.extend([f"{year}{month:0>2}" for month in range(1, 13)])
+        return result
+
+
+class Frequency(MMEnum):
+    ANNUAL = "A"
+    MONTHLY = "M"  # Was not able to get any data from 2008 USA
+
+
+class TradeFlow(MMEnum):
+    # https://comtrade.un.org/data/cache/tradeRegimes.json
+    ALL = "all"
+    IMPORTS = "1"
+    EXPORTS = "2"
+    RE_EXPORTS = "3"  # TODO what is this?
+    RE_IMPORTS = "4"
+
+
+class ClassificationCode(MMEnum):
     """
-    ComtradeClient implements a subset of UNs Public API https://comtrade.un.org/data/doc/api/#APIKey
-
-    BEWARE of the following usage limits:
-    - Rate limit (guest): 1 request every second (per IP address or authenticated user).
-    - Rate limit (authenticated): 1 request every second (per IP address or authenticated user).
-    - Usage limit (guest): 100 requests per hour (per IP address or authenticated user).
-    - Usage limit (authenticated): 10,000 requests per hour (per IP address or authenticated user).
-
-    Parameter combination limits:
-    - ps, r and p are limited to 5 codes each.
-    - Only one of the above codes may use the special ALL value in a given API call.
-    - Classification codes (cc) are limited to 20 items. ALL is always a valid classification code.
+    See classification code availability by partner / year: https://comtrade.un.org/db/mr/daReportersResults.aspx
     """
-    API_BASE_URL = "https://comtrade.un.org/api/get"
-
-    # You can also pass in "YYYY", "YYYYMM" like 201911
-    class Period(MMEnum):
-        NOW = "now"
-        RECENT = "recent"
-        ALL = "all"
-
-        @staticmethod
-        def generate_years(start=MIN_YEAR, end=MAX_YEAR):
-            return [str(year) for year in range(start, end+1, 1)]
-
-        @staticmethod
-        def generate_yearmonths(start_year=MIN_YEAR, end_year=MAX_YEAR):
-            result = []
-            for year in ComtradeClient.Period.generate_years(start_year, end_year):
-                result.extend([f"{year}{month:0>2}" for month in range(1, 13)])
-            return result
-
-    class Frequency(MMEnum):
-        ANNUAL = "A"
-        MONTHLY = "M"  # Was not able to get any data from 2008 USA
-
-    class TradeFlow(MMEnum):
-        # https://comtrade.un.org/data/cache/tradeRegimes.json
-        ALL = "all"
-        IMPORTS = "1"
-        EXPORTS = "2"
-        RE_EXPORTS = "3"  # TODO what is this?
-        RE_IMPORTS = "4"
-
-    class ClassificationCode(MMEnum):
-        """
-        See classification code availability by partner / year: https://comtrade.un.org/db/mr/daReportersResults.aspx
-        """
-        HS = "HS"
-        HS_0_1992 = "H0"
-        HS_1_1996 = "H1"
-        HS_2_2002 = "H2"
-        HS_3_2007 = "H3"
-        HS_4_2012 = "H4"
-        SITC = "ST"
-        """
-        WARNING: only older data was submitted to UN Comtrade in SITC. With rare exceptions no data will be returned
-        in queries from 1992 or later if code ST, meaning "Standard International Trade Classification (SITC),
-        as reported" is chosen. If you require data formatted in the SITC classification,
-        it is better to use S1 for SITC Rev. 1.
-        """
-        SITC_1 = "S1"
-        SITC_2 = "S2"
-        SITC_3 = "S3"
-        SITC_4 = "S4"
-        BEC = "BEC"
-        EB02 = "EB02"
-
-    # TODO: Many more, depends on ClassificationCode, also services.
-    class CommodityCode(MMEnum):
-        AGGREGATED = "total"  # aggregated
-        ALL = "all"
-        AG1 = "AG1"  # one-digit HS commodity codes
-        AG2 = "AG2"  # two-digit HS commodity codes
-        AG3 = "AG3"  # three-digit HS commodity codes  # This doesn't seem to get any results.
-        AG4 = "AG4"  # four-digit HS commodity codes  # This is too much for USA 2008
-        AG5 = "AG5"  # five-digit HS commodity codes
-        AG6 = "AG6"  # six-digit HS commodity codes  # This is too much for Slovakia 2013
-
-    class OutputFormat(MMEnum):
-        # USE_REQUEST_HEADER = None
-        CSV = "csv"
-        JSON = "json"
-
-    class TradeType(MMEnum):
-        COMMODITIES = "C"
-        SERVICES = "S"
-
-    class HeadingFormat(MMEnum):
-        HUMAN_READABLE = "H"
-        MACHINE_READABLE = "M"  # Matches JSON field names.
-
-    def get_trade_data(
-            self,
-            output_filepath: str,
-            partner="703",
-            frequency=Frequency.ANNUAL,
-            period=Period.NOW,
-            classification_code=CommodityCode.AG4,
-            row_limit=100000
-    ):
-        """
-            Queries the UN COMTRADE GET API for the requested arguments and stores it locally into `output_filepath`.
-            More features to come (see TODOs) around.
-
-            :param frequency: TODO
-            :param output_filepath: where to output the downloaded dataset in case of success.
-            :param partner: UN country code of which trade data is requested
-            :param period: See ComtradeClient.Period for possible values, note ALL can lead to "query too complex".
-            :param classification_code: TODO
-            :param row_limit: UN says that max is 100 000: https://comtrade.un.org/data/dev/portal#subscription
-        """
-        query_params = {
-            # reporting area: WHO reported the trade to UNSD.
-            "r": "all",
-            # partner area.The area receiving the trade, based on the reporting areas data.
-            "p": partner,
-            "freq": frequency,
-            "ps": period,
-            "px": ComtradeClient.ClassificationCode.HS,  # classification scheme used
-            "cc": classification_code,
-            "rg": ComtradeClient.TradeFlow.ALL,  # imports / exports
-            "type": ComtradeClient.TradeType.COMMODITIES,
-            "fmt": ComtradeClient.OutputFormat.JSON,  # TODO: Consider CSV, it's more compact than JSON.
-            "max": row_limit,
-            "head": ComtradeClient.HeadingFormat.MACHINE_READABLE,
-        }
-        url = ComtradeClient.API_BASE_URL + "?" + urlencode(query_params)
-        LOGGER.info(f"Sending GET request for url {url}")
-
-        # TODO: Add a requests utils, and collect some metrics on time it takes.
-        # E.g. https://comtrade.un.org/api/get?r=all&p=703&freq=A&ps=2006&px=HS&cc=AG6&rg=all&type=C&fmt=json&max=100000&head=M
-        # took a whopping 1258 seconds (91872 item count), although usually finishes in 100-200 seconds for AG6.
-        start = time.time()
-        try:
-            response = requests.get(url)
-        except ConnectionError as e:
-            raise ComtradeRetriableException(e)
-
-        LOGGER.info(
-            f"  Received response {response.status_code} size={len(response.content)} in {time.time() - start} seconds"
-        )
-
-        dataset = self._parse_dataset(response, query_params)
-        with open(output_filepath, "w") as output_file:
-            json.dump(dataset, output_file)
-
-    # TODO: Work on retry-able errors (like timeouts, rate limits and so).
-    # TODO: Introduce specific errors.
-    def _parse_dataset(self, response: requests.Response, query_params):
-        if response.status_code != HTTPStatus.OK:
-            # For 409 raw response data looks like: b'U\x00S\x00A\x00G\x00E\x00 \x00L\x00I\x00M\x00I\x00T\x00:\
-            # trying decoding: https://stackoverflow.com/questions/4735566/python-unicode-problem
-            response_snippet = response.text[:100]
-            err_str = f"HTTP {response.status_code}: {response_snippet}"
-            if response.status_code in ComtradeRetriableException.list_http_codes():
-                raise ComtradeRetriableException(err_str)
-            raise ComtradeException(err_str)
-
-        content = json.loads(response.content)
-        if "validation" not in content:
-            raise ComtradeRetriableException(f"Validation object expected in response: {content}")
-
-        v = content["validation"]
-
-        # Maybe we can do sth with these.
-        item_count = v["count"]["value"]
-        if item_count > query_params["max"]:
-            # This possibly leads into error 5003
-            # TODO: Figure out if there is pagination, if Yes how to follow, if not how to bypass.
-            LOGGER.warning(f"item_count higher than max: {item_count} > {query_params['max']}")
-        if item_count == 0:
-            LOGGER.warning("item_count is zero, this is likely unexpected")
-
-        validation_status = v["status"]["value"]
-        if validation_status != 0:  # name = "ok"
-            LOGGER.warning(f"Non-zero validation.status: {v}")
-            if validation_status in ComtradeResultTooLarge.list_comtrade_error_codes():
-                raise ComtradeResultTooLarge(f"ResultTooLarge: item count: {item_count}: {v['message']}")
-            elif validation_status in ComtradeInvalidParameters.list_comtrade_error_codes():
-                raise ComtradeInvalidParameters(f"{validation_status}: {v['message']}")
-            else:
-                raise ComtradeException(f"Non-classified error: {validation_status}")
-
-        query_duration = v["count"]["durationSeconds"]
-        dataset_timer = v["datasetTimer"]["durationSeconds"] if "datasetTimer" in v and v["datasetTimer"] else None
-
-        LOGGER.info(
-            f"  Validated response, item count: {item_count}, UN query time: {query_duration}, " +
-            f"UN datasetTimer: {dataset_timer}",
-        )
-        return content["dataset"]
+    HS = "HS"
+    HS_0_1992 = "H0"
+    HS_1_1996 = "H1"
+    HS_2_2002 = "H2"
+    HS_3_2007 = "H3"
+    HS_4_2012 = "H4"
+    SITC = "ST"
+    """
+    WARNING: only older data was submitted to UN Comtrade in SITC. With rare exceptions no data will be returned
+    in queries from 1992 or later if code ST, meaning "Standard International Trade Classification (SITC),
+    as reported" is chosen. If you require data formatted in the SITC classification,
+    it is better to use S1 for SITC Rev. 1.
+    """
+    SITC_1 = "S1"
+    SITC_2 = "S2"
+    SITC_3 = "S3"
+    SITC_4 = "S4"
+    BEC = "BEC"
+    EB02 = "EB02"
 
 
+# TODO: Many more, depends on ClassificationCode, also services.
+class CommodityCode(MMEnum):
+    AGGREGATED = "total"  # aggregated
+    ALL = "all"
+    AG1 = "AG1"  # one-digit HS commodity codes
+    AG2 = "AG2"  # two-digit HS commodity codes
+    AG3 = "AG3"  # three-digit HS commodity codes  # This doesn't seem to get any results.
+    AG4 = "AG4"  # four-digit HS commodity codes  # This is too much for USA 2008
+    AG5 = "AG5"  # five-digit HS commodity codes
+    AG6 = "AG6"  # six-digit HS commodity codes  # This is too much for Slovakia 2013
 
-# TODO: Result processing
-#     {
-#       "pfCode": "H5",
-#       "yr": 2019,
-#       "period": 2019,
-#       "periodDesc": "2019",
-#       "aggrLevel": 0,
-#       "IsLeaf": 0,
-#       "rgCode": 1,
-#       "rgDesc": "Import",
-#       "rtCode": 842,
-#       "rtTitle": "USA",
-#       "rt3ISO": "USA",
-#       "ptCode": 0,
-#       "ptTitle": "World",
-#       "pt3ISO": "WLD",
-#       "ptCode2": null,
-#       "ptTitle2": "",
-#       "pt3ISO2": "",
-#       "cstCode": "",
-#       "cstDesc": "",
-#       "motCode": "",
-#       "motDesc": "",
-#       "cmdCode": "TOTAL",
-#       "cmdDescE": "All Commodities",
-#       "qtCode": 1,
-#       "qtDesc": "No Quantity",
-#       "qtAltCode": null,
-#       "qtAltDesc": "",
-#       "TradeQuantity": 0,
-#       "AltQuantity": null,
-#       "NetWeight": 0,
-#       "GrossWeight": null,
-#       "TradeValue": 2567492197103,
-#       "CIFValue": null,
-#       "FOBValue": null,
-#       "estCode": 4
-#     },
+
+class OutputFormat(MMEnum):
+    # USE_REQUEST_HEADER = None
+    CSV = "csv"
+    JSON = "json"
+
+
+class TradeType(MMEnum):
+    COMMODITIES = "C"
+    SERVICES = "S"
+
+
+class HeadingFormat(MMEnum):
+    HUMAN_READABLE = "H"
+    MACHINE_READABLE = "M"  # Matches JSON field names.
+
+
+def create_url(
+        partner="703",
+        frequency=Frequency.ANNUAL,
+        period=Period.NOW,
+        classification_code=CommodityCode.AG4,
+):
+    """
+        Queries the UN COMTRADE GET API for the requested arguments and stores it locally into `output_filepath`.
+        More features to come (see TODOs) around.
+
+        :param frequency: TODO
+        :param output_filepath: where to output the downloaded dataset in case of success.
+        :param partner: UN country code of which trade data is requested
+        :param period: See ComtradeClient.Period for possible values, note ALL can lead to "query too complex".
+        :param classification_code: TODO
+        :param row_limit: UN says that max is 100 000: https://comtrade.un.org/data/dev/portal#subscription
+    """
+    query_params = {
+        # reporting area: WHO reported the trade to UNSD.
+        "r": "all",
+        # partner area.The area receiving the trade, based on the reporting areas data.
+        "p": partner,
+        "freq": frequency,
+        "ps": period,
+        "px": ClassificationCode.HS,  # classification scheme used
+        "cc": classification_code,
+        "rg": TradeFlow.ALL,  # imports / exports
+        "type": TradeType.COMMODITIES,
+        "fmt": OutputFormat.JSON,  # TODO: Consider CSV, it's more compact than JSON.
+        "max": ROW_LIMIT,
+        "head": HeadingFormat.MACHINE_READABLE,
+    }
+    return API_BASE_URL + "?" + urlencode(query_params)
+
+
+def parse_dataset_from_response(response: requests.Response):
+    if response.status_code != HTTPStatus.OK:
+        # For 409 raw response data looks like: b'U\x00S\x00A\x00G\x00E\x00 \x00L\x00I\x00M\x00I\x00T\x00:\
+        # trying decoding: https://stackoverflow.com/questions/4735566/python-unicode-problem
+        response_snippet = response.text[:100]
+        err_str = f"HTTP {response.status_code}: {response_snippet}"
+        if response.status_code in ComtradeRetriableException.list_http_codes():
+            raise ComtradeRetriableException(err_str)
+        raise ComtradeException(err_str)
+
+    content = json.loads(response.content)
+    if "validation" not in content:
+        raise ComtradeRetriableException(f"Validation object expected in response: {content}")
+
+    v = content["validation"]
+
+    # Maybe we can do sth with these.
+    item_count = v["count"]["value"]
+    if item_count > ROW_LIMIT:
+        LOGGER.warning(f"item_count higher than max: {item_count} > {ROW_LIMIT}")
+    if item_count == 0:
+        LOGGER.warning("item_count is zero, this is likely unexpected")
+
+    validation_status = v["status"]["value"]
+    if validation_status != 0:  # name = "ok"
+        LOGGER.warning(f"Non-zero validation.status: {v}")
+        if validation_status in ComtradeResultTooLarge.list_comtrade_error_codes():
+            raise ComtradeResultTooLarge(f"ResultTooLarge: item count: {item_count}: {v['message']}")
+        elif validation_status in ComtradeInvalidParameters.list_comtrade_error_codes():
+            raise ComtradeInvalidParameters(f"{validation_status}: {v['message']}")
+        else:
+            raise ComtradeException(f"Non-classified error: {validation_status}")
+
+    query_duration = v["count"]["durationSeconds"]
+    dataset_timer = v["datasetTimer"]["durationSeconds"] if "datasetTimer" in v and v["datasetTimer"] else None
+
+    LOGGER.info(
+        f"  Validated response, item count: {item_count}, UN query time: {query_duration}, " +
+        f"UN datasetTimer: {dataset_timer}",
+    )
+    return content["dataset"]
+
 
 
 
--- comtrade/main.py
+++ comtrade/main.py
@@ -1,14 +1,17 @@
-# TODO: Make it a part of ComtradeClient (e.g. the get_trade_data state / parameters are getting shady).
+# TODO: Manage to get all the data (learn to run from multiple IPs).
 # TODO: Add black formatting.
-# TODO: Use threadpools (instead of running it in multiple shells, note the shared rate limiting).
-# TODO: Use multiple IP Proxies for a more massive parallelization (to likely hit my frugal Comcast limits).
+# TODO: Eventually: Use threadpools (instead of running it in multiple shells, note the shared rate limiting).
+# TODO: Eventually: Use multiple IP Proxies for a more massive parallelization (to likely hit my frugal Comcast limits).
 
+import json
 import logging
 import os
+import requests
 import sys
 import time
 
-from comtrade_client import ComtradeClient, ComtradeRetriableException, ComtradeResultTooLarge
+import comtrade_client
+
 from enums.country import Country
 from utils import print_and_sleep, safe_mkdir
 
@@ -20,71 +23,117 @@ logging.basicConfig(
 )
 LOGGER = logging.getLogger("Controller")
 
-# TODO: Make a fancier rate-limiting lib once it's needed more (more scrapers to come).
-start = time.time()
-request_count = 0
-request_at = []
 
-cc_to_try = [ComtradeClient.CommodityCode.AG4, ComtradeClient.CommodityCode.AG2]
-client = ComtradeClient()
-safe_mkdir("data")
-for partner in Country.list_g20():
-    if partner in [Country.ALL]:
-        LOGGER.info(f"Skipping blacklisted country {partner.name}")
-        continue
-    LOGGER.info(f"============ Fetching country {partner.name} ============")
+def get_output_filepath(partner, period, **kwargs):
+    safe_mkdir("data")
 
     directory = f"data/{partner.name.lower()}"
     safe_mkdir(directory)
 
-    # When going through years incrementally, it's very unlikely that a more granular classification would work
-    # in T + N, if it didn't in T. So reset it for each country, and only keep it while it works.
-    cc_i = 0
-    classification_code = cc_to_try[cc_i]
-    for period in ComtradeClient.Period.generate_years(2007, 2018):
-        output_filepath = f"{directory}/{period}.json"
-        # Don't override existing (if messed up, you have to manually remove the file)
-        if os.path.exists(output_filepath):
-            LOGGER.info(f"Skipping existing {output_filepath}")
-            continue
+    output_filepath = f"{directory}/{period}.json"
+    # Idempotency check: Don't override existing (if messed up, you have to manually remove the file).
+    if os.path.exists(output_filepath):
+        return None
+    return output_filepath
 
-        def get_trade_data(cc, retry_count=2):
-            global request_at, request_count
-            request_count += 1
-            now = time.time()
-            request_at.append(now)
-            # Note the inside part of () is a generator. Minor optimization as list comprehension runs in a new func.
-            request_count_last_hour = len(list(t for t in request_at if t > now - 3600))
-            LOGGER.info(f"RateLimit Hint: requests last hour: {request_count_last_hour}")
 
-            try:
-                client.get_trade_data(
-                    output_filepath=output_filepath,
-                    partner=partner,
-                    frequency=ComtradeClient.Frequency.ANNUAL,
-                    period=period,
-                    classification_code=cc,
-                )
-            except ComtradeRetriableException as e:
-                if retry_count > 0:
-                    LOGGER.info(f"Retrying {retry_count} times for {partner.name} for {period} as exception: {e}")
-                    # There are odds we got rate-limited, so chill out for a while.
-                    print_and_sleep(2 ** (2 - retry_count) * 300)
-                    get_trade_data(cc, retry_count=retry_count - 1)
-                else:
-                    LOGGER.info(f"Retry failed, so giving up on {partner.name} for {period} as exception: {e}")
-                    # This doesn't create the data file, so a re-run can fill it in.
+# TODO: Create a fancier rate-limiting library.
+request_count = 0
+request_at = []
 
+
+# The HOW
+def scrape(params):
+    global request_at, request_count
+
+    # 1. Idempotency check
+    output_filepath = get_output_filepath(**params)
+    if output_filepath is None:
+        LOGGER.info(f"  Skipping as output_filepath {output_filepath} is already present.")
+        return
+
+    # 2. Stats / Rate limit
+    request_count += 1
+    start = time.time()
+    request_at.append(start)
+    # Note the inside part of () is a generator. Minor optimization as list comprehension runs in a new func.
+    request_count_last_hour = len(list(t for t in request_at if t > start - 3600))
+    LOGGER.info(f"  RateLimit Hint: requests last hour: {request_count_last_hour}")
+
+    # 3. Get Url
+    url = comtrade_client.create_url(**params)
+    LOGGER.info(f"  Sending GET request for url {url}")
+
+    # 4. Query for data
+    try:
+        response = requests.get(url)
+    except ConnectionError as e:
+        raise comtrade_client.ComtradeRetriableException(e)
+    # TODO: Pretty print content size
+    LOGGER.info(f"  Received {response.status_code} size={len(response.content)} in {time.time() - start} seconds")
+
+    # 5. Parse the dataset
+    dataset = comtrade_client.parse_dataset_from_response(response)
+
+    # 6. Store the dataset
+    with open(output_filepath, "w") as output_file:
+        json.dump(dataset, output_file)
+
+
+def scrape_with_retry(partner, period, classification_code, retry_count=0):
+    params = {
+        "partner": partner,
+        "period": period,
+        "classification_code": classification_code
+    }
+    for attempt in range(retry_count + 1):
         try:
-            get_trade_data(classification_code)
-        except ComtradeResultTooLarge as e:
-            cc_previous = classification_code
-            cc_i = min(len(cc_to_try) - 1, cc_i + 1)
-            classification_code = cc_to_try[cc_i]
-            LOGGER.info(f"Falling back from {cc_previous} to {classification_code} as {e}")
-            # no change
-            if cc_previous == classification_code:
-                LOGGER.error(f"Cannot get data for {partner.name} for {period} as no where to fall back.")
+            LOGGER.info(f"{partner.name}:{period} Scrape attempt {attempt+1}/{retry_count+1} with params {params}")
+            scrape(params)
+            break  # Success, nothing to do.
+        except comtrade_client.ComtradeRetriableException as e:
+            if attempt < retry_count:
+                LOGGER.warning(f"{partner.name},{period}: Retrying {retry_count - attempt} more times as exception: {e}")
+                # There are odds we got rate-limited, so exponentially chill out for a while.
+                print_and_sleep(2 ** attempt * 300)
             else:
-                # TODO: Support multiple fall-backs.
-                get_trade_data(classification_code)
+                LOGGER.error(f"{partner.name},{period}: Retry failed, giving up. Exception: {e}")
+                # This doesn't create the data file, so a re-run can fill it in.
+
+
+# The WHAT
+def scrape_all():
+    # In case higher granularity is needed (likely not worth the 100-1500 second latency).
+    # cc_to_try = [
+    #   comtrade_client.CommodityCode.AG6,
+    #   comtrade_client.CommodityCode.AG4,
+    #   comtrade_client.CommodityCode.AG2,
+    # ]
+    # E.g. https://comtrade.un.org/api/get?r=all&p=703&freq=A&ps=2006&px=HS&cc=AG6&rg=all&type=C&fmt=json&max=100000&head=M
+    # took a whopping 1258 seconds (91872 item count), although usually finishes in 100-200 seconds for AG6.
+    cc_to_try = [comtrade_client.CommodityCode.AG2]
+    for partner in Country.list_g20():
+        if partner in [Country.ALL]:
+            LOGGER.info(f"{partner.name}: Skipping blacklisted country")
+            continue
+        LOGGER.info(f"{partner.name}:========= START ==========")
+
+        # Reset ClassificationCode granularity for every country, and if needed make it less granular to
+        # fit into the row limit for the rest of the years.
+        cc_i = 0
+        for period in comtrade_client.Period.generate_years(2007, 2018):
+            while cc_i < len(cc_to_try):
+                try:
+                    scrape_with_retry(partner, period, cc_to_try[cc_i], retry_count=2)
+                    break  # Success, nothing to do.
+                except comtrade_client.ComtradeResultTooLarge as e:
+                    LOGGER.info(f"  Lowering granularity as {e}")
+                    cc_i += 1
+
+            if cc_i >= len(cc_to_try):
+                LOGGER.error(f"C{partner.name}:{period} Cannot get data for as ResultTooLarge for every granularity.")
+                cc_i = len(cc_to_try) - 1
+
+
+# The DO IT
+scrape_all()
